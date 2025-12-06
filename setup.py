63f76e7d356a8a44d0d073677799c53ceb1148b5
malikhimanshu-01 <himanshumalik2292003@gmail.com>
2025-12-06 10:05:35 +0530

    Project structure ready , enve ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Himanshu Malik",
+    author_email="himanshumalik2292003@gmail.com",
+    packages=find_packages()
+)
